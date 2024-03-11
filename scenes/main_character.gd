2b70c81afc30dd80135599b39750c7e732997066
Zer0x739 <76940003+Zer0x739@users.noreply.github.com>
2024-03-11 18:44:25 +0100

    Add files via upload

--- scenes/main_character.gd
+++ scenes/main_character.gd
@@ -1,31 +1,40 @@
 extends CharacterBody2D
 
+@export var SPEED = 300.0
 
-const SPEED = 100.0
-const JUMP_VELOCITY = -200.0
+@export var jump_height : float
+@export var jump_time_to_peak : float
+@export var jump_time_to_descent : float
+
+@onready var jump_velocity : float = ((2.0 * jump_height) / jump_time_to_peak) * -1.0
+@onready var jump_gravity : float = ((-2.0 * jump_height) / (jump_time_to_peak * jump_time_to_peak)) * -1.0
+@onready var fall_gravity : float = ((-2.0 * jump_height) / (jump_time_to_descent * jump_time_to_descent)) * -1.0
+
 @onready var sprite_2d = $Sprite2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+func get_gravity() -> float:
+	if jump_height < 0.1:
+		print("jump_height nesmi byt mensi nez 0.1")
+	return jump_gravity if velocity.y < 0.0 else fall_gravity
 
 func _physics_process(delta):
 	#Animations
-	if (velocity.x > 1 || velocity.x <-1):
+	if (velocity.x > 1 or velocity.x <-1):
 		sprite_2d.animation = "running"
 	else:
 		sprite_2d.animation = "default"
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		velocity.y += get_gravity() * delta
 		sprite_2d.animation = "jumping"
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = jump_velocity
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
@@ -36,4 +45,3 @@ func _physics_process(delta):
 	
 	var isLeft = velocity.x < 0
 	sprite_2d.flip_h = isLeft
-	
