a02204a38e1bbcf81439585bbb701cd73f1c985d
Zer0x739 <76940003+Zer0x739@users.noreply.github.com>
2024-03-19 16:54:07 +0100

    Delete scenes/transition.gd

--- scenes/transition.gd
+++
@@ -1,7 +0,0 @@
-extends CanvasLayer
-
-func change_scene(target: String) -> void:
-	$AnimationPlayer.play('dissolve')
-	await $AnimationPlayer.animation_finished
-	get_tree().change_scene_to_file("res://scenes/menu.tscn")
-	$AnimationPlayer.play_backwards('dissolve')
